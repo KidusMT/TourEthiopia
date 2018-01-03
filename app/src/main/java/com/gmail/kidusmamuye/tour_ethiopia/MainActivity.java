62d1e8e5ae3cdc6c7e27e46308d382ba62385542
KidusMT <adu.kidus@yahoo.com>
2018-01-03 07:23:17 +0300

    First Commit

---
+++ app/src/main/java/com/gmail/kidusmamuye/tour_ethiopia/MainActivity.java
@@ -0,0 +1,35 @@
+package com.gmail.kidusmamuye.tour_ethiopia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.mapView);
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
+        mMapView.setMap(map);
+    }
+
+    @Override
+    protected void onPause(){
+        mMapView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
+    }
+}
